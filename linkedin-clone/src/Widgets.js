d7d2c5d2c97c477ef7a1e2b2137319af949995ce
hellskater <ksrinivasrao531@gmail.com>
2021-09-29 13:36:35 +0530

    added widgets

--- linkedin-clone/src/Widgets.js
+++ linkedin-clone/src/Widgets.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Widgets.css";
+import InfoIcon from "@mui/icons-material/Info";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
   return (
     <div className="widgets">
-      <h1>Widgets section</h1>
+      <div className="widgets__header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
+      {newsArticle("Rats in your pockets", "Be careful what you fish for")}
     </div>
   );
 }
